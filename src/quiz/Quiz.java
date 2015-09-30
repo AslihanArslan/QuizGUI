42f18f9493339aaabbb122441b249af912e4b8be
AslihanArslan <aslihanarslan26@windowslive.com>
2015-09-30 16:05:55 +0300

    last commit

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -5,16 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
+import javax.xml.soap.Text;
 import javax.swing.JButton;
+import javax.naming.spi.DirStateFactory.Result;
 import javax.swing.ButtonGroup;
 import java.awt.event.ActionListener;
+import java.awt.event.TextEvent;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class Quiz {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private final ButtonGroup buttonGroup_1 = new ButtonGroup();
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -53,17 +58,17 @@ public class Quiz {
 		frame.getContentPane().add(lblNewLabel);
 		
 		JRadioButton rdbtnAnkara = new JRadioButton("Ankara");
-		buttonGroup_1.add(rdbtnAnkara);
+		buttonGroup.add(rdbtnAnkara);
 		rdbtnAnkara.setBounds(6, 52, 66, 23);
 		frame.getContentPane().add(rdbtnAnkara);
 		
 		JRadioButton rdbtnIstanbul = new JRadioButton("Istanbul");
-		buttonGroup_1.add(rdbtnIstanbul);
+		buttonGroup.add(rdbtnIstanbul);
 		rdbtnIstanbul.setBounds(89, 52, 66, 23);
 		frame.getContentPane().add(rdbtnIstanbul);
 		
 		JRadioButton rdbtnMugla = new JRadioButton("Mugla");
-		buttonGroup_1.add(rdbtnMugla);
+		buttonGroup.add(rdbtnMugla);
 		rdbtnMugla.setBounds(166, 52, 109, 23);
 		frame.getContentPane().add(rdbtnMugla);
 		
@@ -76,7 +81,7 @@ public class Quiz {
 		rdbtnAnkara_1.setBounds(6, 109, 66, 23);
 		frame.getContentPane().add(rdbtnAnkara_1);
 		
-		JRadioButton rdbtnIzmir = new JRadioButton("\u0130zmir");
+		JRadioButton rdbtnIzmir = new JRadioButton("Izmir");
 		buttonGroup_1.add(rdbtnIzmir);
 		rdbtnIzmir.setBounds(89, 108, 66, 23);
 		frame.getContentPane().add(rdbtnIzmir);
@@ -89,7 +94,23 @@ public class Quiz {
 		JButton btnEvaluate = new JButton("Evaluate");
 		btnEvaluate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				/*Object selected */ buttonGroup.getSelection().getSelectedObjects();
+				
+				
+				int count = 0;
+				buttonGroup.getSelection().getSelectedObjects();
+			
+				if(rdbtnAnkara.isSelected()){
+					count++;
+				}
+				if(rdbtnMugla_1.isSelected()){
+					count++;
+				}
+				
+				int result =count;
+				
+				String strResult=Integer.toString(result);
+				textField.setText(strResult);
+				
 			}
 		});
 		btnEvaluate.setBounds(30, 205, 89, 23);
@@ -98,5 +119,10 @@ public class Quiz {
 		JLabel lblResult = new JLabel("Result:");
 		lblResult.setBounds(166, 209, 46, 14);
 		frame.getContentPane().add(lblResult);
+		
+		textField = new JTextField();
+		textField.setBounds(222, 206, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
 }
